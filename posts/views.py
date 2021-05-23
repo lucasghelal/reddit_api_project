2fcf34144abcba4c3ae3862a7e4119a8cbf0d557
Helal <lucasghelal@gmail.com>
2021-05-23 12:15:15 -0300

    Deleting and counting votes

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, mixins, permissions, status
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
 
@@ -13,7 +14,7 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
         
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
     
@@ -26,3 +27,10 @@ class VoteCreate(generics.CreateAPIView):
         if self.get_queryset().exists():
             raise ValidationError("You have already voted for this post :)")
         serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk'])) 
+        
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError("You nevervoted for this post.. silly!")
--- reddit_api/urls.py
+++ reddit_api/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('api/posts', views.PostList.as_view()),
     path('api/posts/<int:pk>/vote', views.VoteCreate.as_view()),
     path('api/posts/<int:pk>/vote', views.VoteCreate.as_view()),
-    path('api-auth/', include('rest_frameworks.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     
 ]
